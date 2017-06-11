b59652948131555d69cf2c13e5e5f29914c31727
Artur Grzes <grzesa@poczta.onet.pl>
2017-06-11 10:30:12 +0200

    6

--- js/node.js
+++ js/node.js
@@ -2,6 +2,10 @@
 
 var pierwszyDiv = document.getElementById('parFirst');
 
-console.log(pierwszyDiv.firstChild);
-console.log(pierwszyDiv.lastChild);
+//console.log(pierwszyDiv.firstChild);
+//console.log(pierwszyDiv.lastChild);
+
+var pierwszyChild = pierwszyDiv.childNodes[0];
+console.log(pierwszyChild);
+console.log(pierwszyChild.nextSibling);
 
