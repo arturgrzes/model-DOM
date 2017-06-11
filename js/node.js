1cab344fb9cb4df7deec08286367af51038cf3fd
Artur Grzes <grzesa@poczta.onet.pl>
2017-06-11 10:17:28 +0200

    3

--- js/node.js
+++ js/node.js
@@ -1,7 +1,6 @@
 'use strict'
 
 var pierwszyDiv = document.getElementById('parFirst');
-console.log(pierwszyDiv);
-console.log(pierwszyDiv.parentNode);
-console.log(pierwszyDiv.parentElement);
+
+console.log(pierwszyDiv.childNodes);
 
