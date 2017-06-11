1797476d4020ebc591e2a3d4505109d776ae3276
Artur Grzes <grzesa@poczta.onet.pl>
2017-06-11 10:13:52 +0200

    2

---
+++ js/node.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var pierwszyDiv = document.getElementById('parFirst');
+console.log(pierwszyDiv);
+console.log(pierwszyDiv.parentNode);
+console.log(pierwszyDiv.parentElement);
+
---
+++ js/znajdowanie elementow.js
@@ -0,0 +1,17 @@
+'use script'
+
+var parFirst = document.getElementById('parFirst');
+console.log(parFirst);
+
+var linki = document.getElementsByClassName('superLink');
+console.log(linki);
+
+var linkiPoTagu = document.getElementsByTagName('a');
+console.log(linkiPoTagu);
+
+var.divPoID = document.querySelector('#parSecond');
+console.log(divPoID);
+
+var linkiPoSelektorze =
+    document.querySelectorAll('.superLink');
+console.log
