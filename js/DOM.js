f1375de8b741321208d42c0d788e7fe5cd1af1b0
Artur Grzes <grzesa@poczta.onet.pl>
2017-06-11 09:31:37 +0200

    1

---
+++ js/DOM.js
@@ -0,0 +1,12 @@
+'use script'
+
+console.log (window);
+console.log (window.innerHeight);
+console.log (window.outerHeight);
+
+
+
+console.log (document);
+console.log (document.documentElement);
+console.log (document.head);
+console.log (document.body);
